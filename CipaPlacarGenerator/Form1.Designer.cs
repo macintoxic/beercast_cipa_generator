74283b92b4c0d33b1315fd9d8ea7735d849653b8
Charles Alves <23336+macintoxic@users.noreply.github.com>
2019-11-28 17:17:15 -0300

    Initial Load

---
+++ CipaPlacarGenerator/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace CipaPlacarGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtRecorde = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtHa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Controls.Add(this.panel3);
+            this.panel5.Controls.Add(this.panel2);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Controls.Add(this.panel1);
+            this.panel5.Controls.Add(this.panel4);
+            this.panel5.Location = new System.Drawing.Point(12, 23);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(875, 595);
+            this.panel5.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(194, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(486, 60);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "ESTAMOS TRABALHANDO ";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtRecorde);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Location = new System.Drawing.Point(92, 335);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(691, 123);
+            this.panel3.TabIndex = 16;
+            // 
+            // txtRecorde
+            // 
+            this.txtRecorde.BackColor = System.Drawing.Color.White;
+            this.txtRecorde.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRecorde.Location = new System.Drawing.Point(446, 25);
+            this.txtRecorde.Name = "txtRecorde";
+            this.txtRecorde.Size = new System.Drawing.Size(100, 66);
+            this.txtRecorde.TabIndex = 4;
+            this.txtRecorde.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Green;
+            this.label7.Location = new System.Drawing.Point(560, 28);
+            this.label7.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 60);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "DIAS";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Green;
+            this.label8.Location = new System.Drawing.Point(16, 28);
+            this.label8.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(416, 60);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "NOSSO RECORDE É DE";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Green;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(34, 17);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(806, 68);
+            this.panel2.TabIndex = 15;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = global::CipaPlacarGenerator.Properties.Resources.beercast_brasil;
+            this.pictureBox2.Location = new System.Drawing.Point(333, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(177, 58);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(205, 266);
+            this.label5.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(465, 60);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "SEM FUGA DE PATRONOS";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtHa);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(250, 157);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 100);
+            this.panel1.TabIndex = 13;
+            // 
+            // txtHa
+            // 
+            this.txtHa.BackColor = System.Drawing.Color.White;
+            this.txtHa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHa.Location = new System.Drawing.Point(118, 16);
+            this.txtHa.Name = "txtHa";
+            this.txtHa.Size = new System.Drawing.Size(100, 66);
+            this.txtHa.TabIndex = 4;
+            this.txtHa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(238, 19);
+            this.label4.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 60);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "DIAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(22, 16);
+            this.label3.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 60);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "HÁ";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Green;
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(33, 481);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(806, 95);
+            this.panel4.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::CipaPlacarGenerator.Properties.Resources.Capturar;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(196, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(11, 0, 11, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(554, 45);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "COLABORE PARA MANTER ESTE ÍNDICE";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Impact", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 624);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 57);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "copiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(23F, 60F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 688);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel5);
+            this.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(11, 14, 11, 14);
+            this.Name = "Form1";
+            this.Text = "Beercast Cipa Gen";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtRecorde;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtHa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ CipaPlacarGenerator/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CipaPlacarGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int width = panel5.Size.Width;
+            int height = panel5.Size.Height;
+
+            Bitmap bm = new Bitmap(width, height);
+            panel5.DrawToBitmap(bm, new Rectangle(0, 0, width, height));
+
+
+            Clipboard.SetImage(bm);
+
+           // bm.Save(@"D:\TestDrawToBitmap.bmp", ImageFormat.Bmp);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+// ConfigurationManager.AppSettings["a"] = 1.ToString();
+            ConfigurationManager.AppSettings.Set("ha", txtHa.Text);
+           
+            //Properties.Settings.Default.Properties.Clear();
+
+            //Properties.Settings.Default.Properties.Add(
+            //    new SettingsProperty("txtHa")
+            //    {
+            //        DefaultValue = txtHa.Text,
+            //        IsReadOnly = false,
+            //        PropertyType = typeof(string),
+            //        Provider = Properties.Settings.Default.Providers["LocalFileSettingsProvider"],
+
+            //    }
+            //    );
+
+            //Properties.Settings.Default.Save();
+            //System.Configuration.SettingsProperty prop = new SettingsProperty("txtHa")
+            //{
+            //    DefaultValue = "Default",
+            //    IsReadOnly = false,
+            //    PropertyType = typeof(string),
+            //    Provider = Properties.Settings.Default.Providers["LocalFileSettingsProvider"],
+
+            //};
+            //prop.Attributes.Add(typeof(UserScopedSettingAttribute), new UserScopedSettingAttribute());
+            //Properties.Settings.Default.Properties.Add(prop);
+
+
+
+            //Properties.Settings.Default.Properties.Add(new SettingsProperty("txtHa")
+            //{
+            //    DefaultValue = "Default",
+            //    IsReadOnly = false,
+            //    PropertyType = typeof(string),
+            //    Provider = Properties.Settings.Default.Providers["LocalFileSettingsProvider"]
+            //});
+
+
+            //Properties.Settings.Default.Properties.Add(new SettingsProperty( "txtHa",  txtHa.Text);
+            //Properties.Settings.Default.Properties.Add ("txtRecorde", txtRecorde.Text);
+            //Properties.Settings.Default.Save();
+            //Properties.Settings.Default["txtRecorde"] = txtRecorde.Text;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var x = Properties.Settings.Default.Properties["txtHa"];
+            txtHa.Text = ConfigurationManager.AppSettings.Get("ha");
+            //if (Properties.Settings.Default.SettingsKey ["txtHa"] != null)
+            //    txtHa.Text = Properties.Settings.Default["txtHa"].ToString();
+        }
+    }
+}
